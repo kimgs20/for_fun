f8a54d4c6b6af4bedbb2308b5d409056e5617430
kgs <kimgeunsukgs@gmail.com>
2022-03-19 20:28:31 +0900

    cylcoid_line

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ this repo save something useless codes.
 
 22-03-15
 ![gif](./gifs/ball_trajectory.gif)
+
+22-03-19
+![gif](./gifs/cycloid_line.gif)
---
+++ cycloid_line.py
@@ -0,0 +1,30 @@
+"""
+cycloid
+
+x = r * (\theta - sin(\theta))
+y = r * (1- cos(\theta))
+"""
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+fig, ax = plt.subplots()
+ax.set_aspect('equal')
+ax.set_xlim(-1, 2 * np.pi + 1)
+ax.set_ylim(-0.5, 2.5)
+
+r = 1
+x, y = [], []
+traj, = plt.plot([], [])
+
+def update(t):
+    x.append(r * (t - np.sin(t)))
+    y.append(r * (1 - np.cos(t)))
+    traj.set_data(x, y)
+    if t > 6.28:
+        sys.exit()
+
+ani = FuncAnimation(fig, update, frames=np.linspace(0, 2 * np.pi))
+# ani.save('cycloid_line.gif')
+plt.show()
